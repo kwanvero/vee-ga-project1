755e96f5e042875e8e72286b6418cec86f6ee528
Vee Mercado <kwanvero@gmail.com>
2016-08-31 19:57:51 +1000

    update

--- scripts/vivibot.js
+++ scripts/vivibot.js
@@ -5,7 +5,7 @@ module.exports = function (robot) {
     let dinner = ['Salad', 'Pizza', 'Tacos', 'Instant noodles', 'Pad Thai', 'Nachos', 'Dumplings', 'Pasta', 'Kibbles', 'a carrot...']
     let bePolite = [' ', 'Possibly', 'Thinking it might be', 'Feeling like', 'Craving', 'Left over', 'Most likely,']
 
-    return res.send(bePolite[Math.floor(Math.random() * (10 - 1) + 1)] + ' ' + dinner[Math.floor(Math.random() * (7 - 1) + 1)]);
+    return res.send(bePolite[Math.floor(Math.random() * (7 - 1) + 1)] + ' ' + dinner[Math.floor(Math.random() * (10 - 1) + 1)]);
 
   });
 }
