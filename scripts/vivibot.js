f2948b60fed54fb651a976a2ed6f20828893d025
Vee Mercado <kwanvero@gmail.com>
2016-08-31 20:15:58 +1000

    update

--- scripts/vivibot.js
+++ scripts/vivibot.js
@@ -1,11 +1,16 @@
 module.exports = function (robot) {
 
-  robot.hear(/What's for dinner?/, function (res) {
+  robot.hear(/What's for dinner?/, function (food) {
 
     let dinner = ['Salad', 'Pizza', 'Tacos', 'Instant noodles', 'Pad Thai', 'Nachos', 'Dumplings', 'Pasta', 'Kibbles', 'a carrot...']
     let bePolite = [' ', 'Possibly', 'Thinking it might be', 'Feeling like', 'Craving', 'Left over', 'Most likely,']
 
-    return res.send(bePolite[Math.floor(Math.random() * (7 - 1) + 1)] + ' ' + dinner[Math.floor(Math.random() * (10 - 1) + 1)]);
+    return food.send(bePolite[Math.floor(Math.random() * (7 - 1) + 1)] + ' ' + dinner[Math.floor(Math.random() * (10 - 1) + 1)])
 
-  });
+  })
+
+  robot.respond(/(.*) play (.*) game (.*)/, function (game) {
+
+    return game.send("I get to be game master!, You sure about this?")
+  })
 }
